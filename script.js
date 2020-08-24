05ba70593e5c021f6814946663c0f094c74ca141
Matheus Franco <matheusefranco@gmail.com>
2020-08-24 17:04:47 -0300

    Add Ex1

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function dolarex1() {
+  var numero1 = document.getElementById("numero1").value;
+  var numero2 = document.getElementById("numero2").value;
+  var resultado = numero2 / numero1;
+  document.getElementById("resultado").value = "U$ " + resultado.toFixed(2);
+}
+//-------------
 function areaex4() {
   var numero1 = document.getElementById("numero1").value;
   var numero2 = document.getElementById("numero2").value;
